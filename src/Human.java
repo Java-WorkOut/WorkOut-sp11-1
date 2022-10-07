3b83e61ed10924932624d9a4616ccc254d6b2b52
Tarasov Sergei <www.neon@mail.ru>
2022-10-07 17:21:18 +0500

    finished task 1-1

--- src/Human.java
+++ src/Human.java
@@ -2,14 +2,16 @@ public class Human {
     private String birthday;
     private String fullName;
     private String city;
+    private String jobTitle;
 
-    public Human(String birthday, String fullName, String city) {
+    public Human(String birthday, String fullName, String city, String jobTitle) {
         this.birthday = birthday;
         this.fullName = fullName;
         this.city = city;
+        this.jobTitle = jobTitle;
     }
 
     public void printMessage() {
-        System.out.printf("Привет! Меня зовут %s. Я из города %s. Я родился в %s году. Будем знакомы!\n", fullName, city, birthday);
+        System.out.printf("Привет! Меня зовут %s. Я из города %s. Я родился в %s году. Я работаю на должности %s. Будем знакомы!\n", fullName, city, birthday, jobTitle);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,10 @@ public class Main {
     }
 
     private static void task_1_1() {
-        Human human1 = new Human("1987", "Максим", "Минск");
-        Human human2 = new Human("1993", "Аня", "Москва");
-        Human human3 = new Human("1994", "Катя", "Калининград");
-        Human human4 = new Human("1995", "Артем", "Москва");
-
+        Human human1 = new Human("1987", "Максим", "Минск", "Бренд-менеджер");
+        Human human2 = new Human("1993", "Аня", "Москва", "Методист образовательных программ");
+        Human human3 = new Human("1994", "Катя", "Калининград", "Продакт-менеджер");
+        Human human4 = new Human("1995", "Артем", "Москва", "Директор по развитию бизнеса");
         human1.printMessage();
         human2.printMessage();
         human3.printMessage();
